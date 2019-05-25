8cf4ffc9d1b34ac74888641c367f587f35e57e62
Max Konovalov <max.kon.38r@gmail.com>
2019-05-25 14:01:00 +0800

    Выволнил 1 и 5 этап

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 import pyaudio
 import wave
 
-# следующий код для записи звука:
-# константы:
+# Константы:
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+RECORD_SECONDS = 20
+#WAVE_OUTPUT_FILENAME = "output.wav"  # !!!
 
-def record_audio(filename):
+# Функция записи:
+def record_audio(filename='speech.wav'):
     p = pyaudio.PyAudio()
 
     stream = p.open(format = FORMAT,
@@ -33,9 +33,40 @@ def record_audio(filename):
     stream.close()
     p.terminate()
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf = wave.open(filename, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
     wf.setframerate(RATE)
     wf.writeframes(b''.join(frames))
     wf.close()
+
+# Функция воспроизведения:
+def play_audio(filename='speech.wav'):
+	wf = wave.open(filename, 'rb')
+
+	p = pyaudio.PyAudio()
+
+	stream = p.open(format = p.get_format_from_width(wf.getsapwidth()),
+		channels = wf.getnchannels(),
+		rate = wf.getframerate(),
+		output = True)
+
+	data = wf.readframes(CHUNK)
+
+	while data:
+		stream.write(data)
+		data = wf.readframes(CHUNK)
+
+	stream.stop_stream()
+	stream.close()
+
+	p.terminate()
+
+#
+def main():
+	record_audio()
+	#sleep()
+	play_audio()
+
+if __name__ == 'main':
+	main()
